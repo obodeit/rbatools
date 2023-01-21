b1ac24ae012bdb369fe01f7db2ae95424050202a
obodeit <38722594+obodeit@users.noreply.github.com>
2023-01-21 19:21:18 +0100

    Update WIP_calib_Parallel_WF.py

--- WIP_calib_Parallel_WF.py
+++ WIP_calib_Parallel_WF.py
@@ -191,7 +191,7 @@ if __name__ == "__main__":
     #warnings.simplefilter('ignore', SettingWithCopyWarning)
     main(n_parallel_processes=3,
         #conditions = ['Hackett_C01']
-        #conditions = ['Hackett_C005', 'Hackett_C01', 'Hackett_C016', 'Hackett_C022', 'Hackett_C03']
-        conditions = ['Hackett_N005', 'Hackett_N01', 'Hackett_N016', 'Hackett_N03']
+        conditions = ['Hackett_C005', 'Hackett_C01', 'Hackett_C016', 'Hackett_C022', 'Hackett_C03']
+        #conditions = ['Hackett_N005', 'Hackett_N01', 'Hackett_N016', 'Hackett_N03']
         #conditions = ['Hackett_P005', 'Hackett_P01', 'Hackett_P016', 'Hackett_P022', 'Hackett_P03']
         )
